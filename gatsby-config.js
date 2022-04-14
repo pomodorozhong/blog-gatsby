8815dfa18874443affd21aac9a4a63659245031d
pomodorozhong <pomodorozhong@gmail.com>
2022-04-14 21:43:30 +0800

    feat✨(gatsby-config.js): setup `siteMetadata`

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `pomodorozhong's Blog`,
+    siteTitleAlt: `pomodorozhong's Blog`,
+    siteHeadline: `pomodorozhong's Blog`,
+    siteUrl: `https://blog-pomodorozhong.vercel.app`,
+    siteDescription: `This blog is about my thoughts on being an indie dev, and also my apps' development log.`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteImage: ``,
+    author: `@pomodorozhong`,
   },
   plugins: [
     {
